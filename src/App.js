9b98a8aa0188c38da308998e53792b5f61a6f5ce
allistermugaisi <allistermugaisi@gmail.com>
2021-06-26 19:49:04 +0300

    added redux as global state, added loading spinner for suspense lazy loading

--- src/App.js
+++ src/App.js
@@ -1,9 +1,41 @@
-import React from 'react';
+import React, { Suspense } from 'react';
+import { Route, Switch } from 'react-router-dom';
+import { Landing } from './components';
+import { LoadingSpinner } from './utils/LoadingSpinner';
+import { ThemeProvider } from '@material-ui/styles';
+import { createMuiTheme } from '@material-ui/core/styles';
+
+const theme = createMuiTheme({
+	palette: {
+		primary: {
+			main: '#069c54',
+		},
+		secondary: {
+			main: '#ff1744',
+		},
+	},
+	typography: {
+		fontFamily: "'Poppins', sans-serif",
+		textTransform: 'none',
+
+		button: {
+			textTransform: 'none',
+		},
+	},
+});
 
 function App() {
 	return (
-		<div className="App">
-			<h1>Afya Emr</h1>
+		<div>
+			<ThemeProvider theme={theme}>
+				<Switch>
+					<Suspense fallback={<LoadingSpinner />}>
+						<Route exact path="/" component={Landing} />
+					</Suspense>
+					{/* <Route path="/signin" component={Login} />
+						<Route path="/payment" component={Payment} /> */}
+				</Switch>
+			</ThemeProvider>
 		</div>
 	);
 }
